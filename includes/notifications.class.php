b3ea369b5b875e78651ee7497d5067256049915d
Jason Gegere <jason@hgmail.com>
2012-12-13 12:00:46 -0600

    Log notifications sent via Checkfront

---
+++ includes/notifications.class.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+
+Logging the notifications sent from Checkfront to guests… So, the process can be improved and we know exactly what is getting sent
+
+*/
+
+class Notification 
+{
+
+	public $data;
+	public $dataArray;
+
+	function __construct(){  
+		$this->data = json_decode(file_get_contents('php://input'));
+    }
+
+
+	public function ParseNotificationData() {
+		
+		$host	 		= $this->data->{'@attributes'}->host;
+		$status 		= $this->data->booking->status;
+		$code 			= $this->data->booking->code;
+		$email_date		= date('Y-m-d H:i:s');
+		$created_date	= $this->data->booking->created_date;
+		$staff_id		= $this->data->booking->staff_id;
+		$source_ip		= ip2long($this->data->booking->source_ip);
+		$start_date		= $this->data->booking->start_date;
+		$end_date		= $this->data->booking->end_date;
+		$name			= $this->data->booking->customer->name;
+		$email			= $this->data->booking->customer->email;
+		$region			= $this->data->booking->customer->region;
+		$address		= $this->data->booking->customer->address;
+		$country		= $this->data->booking->customer->country[0];
+		$postal_zip		= $this->data->booking->customer->postal_zip;
+		
+		
+		$this->dataArray = array('host'			=> $host,
+								'status' 		=> $status, 
+								'code' 			=> $code, 
+								'email_date' 	=> $email_date,
+								'created_date' 	=> $created_date, 
+								'staff_id' 		=> $staff_id, 
+								'source_ip' 	=> $source_ip, 
+								'start_date' 	=> $start_date,
+								'end_date' 		=> $end_date, 
+								'name' 			=> $name,
+								'email' 		=> $email, 
+								'region' 		=> $region,
+								'address' 		=> $address, 
+								'country' 		=> $country,
+								'postal_zip' 	=> $postal_zip,
+								'raw_data'		=> serialize($this->data)
+								);
+
+    }
+	
+}
+
+?>
