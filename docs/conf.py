82c7cf37168296a66eaa53c693930cfbb41e360b
Kris Lemberg <kris@checkfront.com>
2015-04-04 12:09:06 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_http_domain']
+extensions = ['sphinxcontrib.httpdomain']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
