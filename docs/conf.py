77c29a18be95d158f7e98f00080b850179bf0d8e
Brett Goss <goss.brett@gmail.com>
2018-06-06 18:15:12 -0700

    Use str instead of unicode for python3 (#19)

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 # General information about the project.
 project = u'Checkfront API'
-copyright = unicode(datetime.datetime.now().year) + u' Checkfront Inc. All rights reserved'
+copyright = str(datetime.datetime.now().year) + u' Checkfront Inc. All rights reserved'
+
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
