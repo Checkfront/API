238f035813cfb6b5a2650f55e783b9f4dca1a6fb
Brett Goss <brett@checkfront.com>
2023-12-19 13:38:23 -0800

    Update config to support new sphinx versions and theming (#40)

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.httpdomain']
+extensions = ['sphinxcontrib.httpdomain', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -102,7 +102,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 if on_rtd:
-    html_theme = 'default'
+    html_theme = "sphinx_rtd_theme"
 else:
     html_theme = "sphinx_rtd_theme"
     html_theme_path = ["_templates", ]
